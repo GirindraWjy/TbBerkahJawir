fb287a90841ec0c8ea5554a16bec7e1b4ca1946c
Girindra Wijaya <wijayaindrapbg@gmail.com>
2023-11-24 10:17:17 +0700

    first commit

---
+++ BerkahJawir/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace BerkahJawir
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.tb_pass = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(196, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(360, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "DATABASE TB BERKAH JAWIR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(264, 49);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Silahkan login untuk menambahkan data";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(328, 78);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(329, 134);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Password";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // tb_username
+            // 
+            this.tb_username.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_username.Location = new System.Drawing.Point(254, 100);
+            this.tb_username.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(217, 23);
+            this.tb_username.TabIndex = 5;
+            this.tb_username.TextChanged += new System.EventHandler(this.tb_username_TextChanged);
+            // 
+            // tb_pass
+            // 
+            this.tb_pass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_pass.Location = new System.Drawing.Point(254, 155);
+            this.tb_pass.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_pass.Name = "tb_pass";
+            this.tb_pass.Size = new System.Drawing.Size(217, 23);
+            this.tb_pass.TabIndex = 6;
+            this.tb_pass.UseSystemPasswordChar = true;
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_login.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_login.Location = new System.Drawing.Point(255, 204);
+            this.btn_login.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(216, 34);
+            this.btn_login.TabIndex = 7;
+            this.btn_login.Text = "Login as Admin";
+            this.btn_login.UseVisualStyleBackColor = false;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-9, -22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(772, 448);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 383);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.tb_pass);
+            this.Controls.Add(this.tb_username);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox tb_username;
+        private TextBox tb_pass;
+        private Button btn_login;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ BerkahJawir/FormInventory.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace BerkahJawir
+{
+    partial class FormInventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormInventory));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.tb_nama = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_harga = new System.Windows.Forms.TextBox();
+            this.tb_stok = new System.Windows.Forms.TextBox();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_edit = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_refresh = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.BurlyWood;
+            this.label1.Location = new System.Drawing.Point(186, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DASHBOARD INVENTORY";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID Produk";
+            // 
+            // tb_id
+            // 
+            this.tb_id.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_id.Location = new System.Drawing.Point(186, 74);
+            this.tb_id.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(290, 23);
+            this.tb_id.TabIndex = 2;
+            // 
+            // tb_nama
+            // 
+            this.tb_nama.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_nama.Location = new System.Drawing.Point(186, 105);
+            this.tb_nama.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_nama.Name = "tb_nama";
+            this.tb_nama.Size = new System.Drawing.Size(290, 23);
+            this.tb_nama.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 105);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nama Produk";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 137);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Harga Produk";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 168);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Stok Produk";
+            // 
+            // tb_harga
+            // 
+            this.tb_harga.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_harga.Location = new System.Drawing.Point(186, 137);
+            this.tb_harga.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_harga.Name = "tb_harga";
+            this.tb_harga.Size = new System.Drawing.Size(290, 23);
+            this.tb_harga.TabIndex = 7;
+            // 
+            // tb_stok
+            // 
+            this.tb_stok.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_stok.Location = new System.Drawing.Point(186, 168);
+            this.tb_stok.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_stok.Name = "tb_stok";
+            this.tb_stok.Size = new System.Drawing.Size(290, 23);
+            this.tb_stok.TabIndex = 8;
+            // 
+            // btn_save
+            // 
+            this.btn_save.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btn_save.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btn_save.Location = new System.Drawing.Point(503, 75);
+            this.btn_save.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(96, 26);
+            this.btn_save.TabIndex = 9;
+            this.btn_save.Text = "SAVE";
+            this.btn_save.UseVisualStyleBackColor = false;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_edit
+            // 
+            this.btn_edit.BackColor = System.Drawing.Color.Peru;
+            this.btn_edit.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btn_edit.Location = new System.Drawing.Point(504, 105);
+            this.btn_edit.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(96, 26);
+            this.btn_edit.TabIndex = 10;
+            this.btn_edit.Text = "EDIT";
+            this.btn_edit.UseVisualStyleBackColor = false;
+            this.btn_edit.Click += new System.EventHandler(this.btn_edit_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.BackColor = System.Drawing.Color.Red;
+            this.btn_delete.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btn_delete.Location = new System.Drawing.Point(504, 137);
+            this.btn_delete.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(96, 26);
+            this.btn_delete.TabIndex = 11;
+            this.btn_delete.Text = "DELETE";
+            this.btn_delete.UseVisualStyleBackColor = false;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(47, 201);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(553, 148);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btn_refresh
+            // 
+            this.btn_refresh.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.btn_refresh.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btn_refresh.Location = new System.Drawing.Point(504, 168);
+            this.btn_refresh.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_refresh.Name = "btn_refresh";
+            this.btn_refresh.Size = new System.Drawing.Size(96, 26);
+            this.btn_refresh.TabIndex = 13;
+            this.btn_refresh.Text = "REFRESH";
+            this.btn_refresh.UseVisualStyleBackColor = false;
+            this.btn_refresh.Click += new System.EventHandler(this.btn_refresh_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MintCream;
+            this.button1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button1.Location = new System.Drawing.Point(504, 366);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 26);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "EXIT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(665, 404);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormInventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 403);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_refresh);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_edit);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.tb_stok);
+            this.Controls.Add(this.tb_harga);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_nama);
+            this.Controls.Add(this.tb_id);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormInventory";
+            this.Text = "FormInventory";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox tb_id;
+        private TextBox tb_nama;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox tb_harga;
+        private TextBox tb_stok;
+        private Button btn_save;
+        private Button btn_edit;
+        private Button btn_delete;
+        private DataGridView dataGridView1;
+        private Button btn_refresh;
+        private Button button1;
+        private PictureBox pictureBox1;
+    }
+}
